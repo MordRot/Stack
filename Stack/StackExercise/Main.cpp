643283898ec3f09caf3501b5b6b3f05ce4d71ff7
mord <mord@cyberElite.jct>
2017-11-15 16:06:14 +0200

    Write Stack program

---
+++ Stack/StackExercise/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "MyStack.h"
+using namespace std;
+
+
+
+void main()
+{
+	StackExercise::MyStack<int> s;
+
+	// ...
+
+}
---
+++ Stack/StackExercise/MyStack.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "StackOverflowException.h"
+#include "StackUnderflowException.h"
+
+namespace StackExercise
+{
+	const int STACK_SIZE = 10;
+
+	template <typename T>
+	class MyStack
+	{
+	public:
+		MyStack();
+
+		void Push(T val);
+		int Pop();
+
+	private:
+		int m_impl[STACK_SIZE];
+		int m_nextIndex;
+	};
+
+
+	template <typename T>
+	MyStack<T>::MyStack()
+		: m_nextIndex(0)
+	{}	
+
+	template <typename T>
+	void MyStack<T>::Push(T val)
+	{
+		if (m_nextIndex < STACK_SIZE)
+		{
+			m_impl[m_nextIndex] = val;
+			++m_nextIndex;
+		}
+		else
+		{
+			throw StackOverflowException();
+		}
+	}
+
+	template <typename T>
+	int MyStack<T>::Pop()
+	{
+		if (m_nextIndex > 0)
+		{
+			--m_nextIndex;
+			return m_impl[m_nextIndex];
+		}
+		else
+		{
+			throw StackUnderflowException();
+		}
+	}
+}
